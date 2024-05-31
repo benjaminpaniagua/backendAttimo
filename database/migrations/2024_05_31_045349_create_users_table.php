5d8ff845407b9e8bb06f4b45e0cd4df875a6cb4f
benjaminpaniagua <benjaminjosepaniaguarojas@gmail.com>
2024-05-30 22:55:12 -0600

    first commit

---
+++ database/migrations/2024_05_31_045349_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname1');
+            $table->string('lastname2');
+            $table->string('email');
+            $table->string('username');
+            $table->string('password');
+            $table->string('image');
+            $table->string('user_type_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
